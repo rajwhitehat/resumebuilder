a1e9d96ba6c50886e858abc82cdd35a4f3b3b6bd
Raj Sharma <rajwhitehat@users.noreply.github.com>
2018-12-04 23:48:23 +0530

    homepage

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    RouterModule,
     AppRoutingModule
+
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
