175e2d80ae68ccf173f21acb6b5704972948a4f1
jyoti <jyoti06goel@gmail.com>
2018-12-12 22:02:40 +0530

    insert db data

---
+++ server/originator/src/main/resources/db/migration/V1.1__insertSubject.sql
@@ -0,0 +1,4 @@
+load data infile "F:/project/spring/workspace/originator/insertData/subject.csv" INTO TABLE subject
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 rows;
---
+++ server/originator/src/main/resources/db/migration/V1.2__insertQuestionType.sql
@@ -0,0 +1,4 @@
+load data infile "F:/project/spring/workspace/originator/insertData/ques_type.csv" INTO TABLE que_type
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 rows;
