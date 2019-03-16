d6f29a783ce93ceb0c8ae2507838ebeb348486ff
jyoti <jyoti06goel@gmail.com>
2019-03-16 20:21:34 +0530

    insert table data during tomcat9 server deploy with absolute path

--- server/originator.server/src/main/resources/db/migration/V1.0__createSchema.sql
+++ server/originator.server/src/main/resources/db/migration/V1.0__createSchema.sql
@@ -2,8 +2,6 @@ create table subject (subject_id bigint(20) auto_increment primary key, subject_
 
 create table que_type (type_id bigint(20) auto_increment primary key, type_name varchar(50) not null);
 
-create table passage_que (passage_id bigint(20) auto_increment primary key, passage text not null);
-
 create table questions (question_id bigint(20) auto_increment primary key, question varchar(255) not null,
 type_id bigint(20) references que_type(type_id), answer varchar(255) not null, is_image bit(1) default 0, subject_id bigint(20) references subject(subject_id));
 
--- server/originator.server/src/main/resources/db/migration/V1.1__insertSubject.sql
+++ server/originator.server/src/main/resources/db/migration/V1.1__insertSubject.sql
@@ -1,4 +1,6 @@
-load data local infile "insertData/subject.csv" INTO TABLE subject 
+load data local infile 
+${subject_insert}
+ INTO TABLE subject 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\r\n' 
 IGNORE 1 rows;
