57f11af1bbfa4643514cc2855f2f573e0f623927
raj.sharma.vinove <raj.sharma@mail.vinove.com>
2019-09-05 17:28:46 +0530

    security

---
+++ server/originator.security.db/src/main/java/com/nytcoder/originator/security/db/UserEntity.java
@@ -0,0 +1,52 @@
+package com.nytcoder.originator.security.db;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long user_id;
+    private String first_name;
+	private String last_name;
+	private String email;
+	private String password;
+	
+	public Long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(Long user_id) {
+		this.user_id = user_id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+}
---
+++ server/originator.security.db/src/main/java/com/nytcoder/originator/security/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.nytcoder.originator.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Object, Object> {
+
+}
